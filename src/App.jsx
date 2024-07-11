3ce6db04ee0f6c9f476937ba21e90ed709fc9418
danielpribeiro <danielpereiraribeiro973@gmail.com>
2024-07-10 21:20:27 -0300

    krl

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,11 @@
 import Titulo from "./titulo"
 
 function App(){
-  return <Titulo/>
+  return <div>
+    <Titulo nome="Dimitri" paragrafo = {true}/>
+    <Titulo/>
+    <Titulo/>
+  </div>
 }
 
 export default App
--- src/titulo.jsx
+++ src/titulo.jsx
@@ -1,5 +1,18 @@
-function Titulo(){
-    return <h1>Oi eu sou um título</h1>
+function Titulo({nome, paragrafo}) {
+    return (
+    <div>
+        <h1>Oi eu sou {nome ? nome : "Fulano"} </h1>
+        <p>Lorem Ipsum Dolor</p>
+         {
+            paragrafo ?
+            <p> My names</p>
+            :
+            <p>Nada</p>
+         }
+         
+        
+    </div>
+    )
 }
 
 export default Titulo
